9409fada2508f22134109cada31d129fce0fb6a0
jaykishankrk <98607222+jaykishankrk@users.noreply.github.com>
2022-08-17 14:35:12 -0400

    Changes made to use source file name for target as well
    Changed save filename to use the source filename with .docx extension. This way the filename will be unique for each file processed.

--- ConvertPDFtoDocx.py
+++ ConvertPDFtoDocx.py
@@ -1,41 +1,52 @@
 import os
-
+import ntpath
 from pdf2docx import Converter
 import tkinter as tk
 from tkinter import filedialog
 
+
+# function to retrieve filename from absolute path.
+def retrieve_filename(path_and_filename):
+    # Split the filename and pathname
+    path_name, file_name = ntpath.split(path_and_filename)
+    # return filename if it exists or else the whole path as is.
+    return file_name or ntpath.basename(path_name)
+
+
 root = tk.Tk()
 root.withdraw()
 
-# print("Current working directory: " + os.getcwd())
-# strCWD = "c:\\temp"
-# print("Setting CWD to : " + strCWD)
-# os.chdir(strCWD)
-
-# pdf_file = input("Please provide the PDF (source) Filename/Filenames : ")
 print("Please provide the PDF (source) Filename/Filenames")
-#pdf_file = filedialog.askopenfilename()
+
+# opens file dialog to select multiple PDF files.
 pdf_files = filedialog.askopenfilenames()
 
-# doc_file = input("Please provide the DOC (target) folder : ")
 print("Please provide the DOC (target) folder")
-doc_file_folder = filedialog.askdirectory()
-counter_var = 1
 
+# opens folder dialog to select target folder path.
+doc_file_folder = filedialog.askdirectory()
+
+# Iterate through list of PDF files selected and convert the same to docx.
 for pdf_file in pdf_files:
+    # check to make sure the files selected are indeed PDF files.
     filename, file_extension = os.path.splitext(pdf_file)
     if file_extension.upper() != ".PDF":
-        print("File : " +  pdf_file + " is not a PDF File, bypassing the file...")
+        print("File : " + pdf_file + " is not a PDF File, bypassing the file...")
         continue
-    doc_file = doc_file_folder + "/result" + str(counter_var) + ".docx"
-    counter_var += 1
-    pdf_filecntrl = open(pdf_file, "r+")
+
+    # get the target filename
+    target_filename = retrieve_filename(filename)
+    doc_file = doc_file_folder + "/" + target_filename + ".docx"
+
+    # check to see if the PDF file is readable or not.
+    pdf_filecntrl = open(pdf_file, "r")
     if not pdf_filecntrl.readable():
         print("PDF File is not readable. Quitting")
         exit(-1)
     else:
         pdf_filecntrl.close()
 
+    # check to see if the target file is writable or not.
     doc_filecntrl = open(doc_file, "w")
     if not doc_filecntrl.writable():
         print("DOC File is not writable. Quitting")
@@ -43,6 +54,9 @@ for pdf_file in pdf_files:
     else:
         doc_filecntrl.close()
 
+    # Steps below are used to convert PDF to DOCX one file at a time.
     convPDFtoDoc = Converter(pdf_file)
     convPDFtoDoc.convert(doc_file)
     convPDFtoDoc.close()
+
+# End of program.
